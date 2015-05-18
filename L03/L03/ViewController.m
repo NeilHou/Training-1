baff6bba193ea8b391e80a00e57aaaaef533fe32
LawrenceHan <yangfei6565@163.com>
2015-05-18 11:50:56 +0800

    Added label

--- L03/L03/ViewController.m
+++ L03/L03/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LHUiview.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
@@ -18,6 +19,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+//    LHUiview *labelView = [[LHUiview alloc] initWithFrame:CGRectMake(10, 10, 160, 160)];
+//    [self.scrollView addSubview:labelView];
+//    labelView.backgroundColor = [UIColor greenColor];
+//    
+//    //
+//    CGRect scrollViewBounds = self.scrollView.bounds;
+//    CGRect labelViewFrame = labelView.frame;
+//    
+//    // Calc width and height
+//    CGFloat labelX = (scrollViewBounds.size.width - labelViewFrame.size.width) / 2;
+//    CGFloat labelY = (scrollViewBounds.size.height - labelViewFrame.size.height) / 2;
+//    
+//    // Set the XY to labelView
+//    CGRect newFrame = CGRectMake(labelX, labelY,
+//                                 labelViewFrame.size.width, labelViewFrame.size.height);
+//    
+//    labelView.frame = newFrame;
+//    
+//    UILabel *numberLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
+//    numberLabel.text = @"1";
+//    [labelView addSubview:numberLabel];
+//    
+//    CGRect numberViewBounds = labelView.bounds;
+//    CGRect numberViewFrame = numberLabel.frame;
+//
+//    CGFloat labelX1 = (numberViewBounds.size.width - numberViewFrame.size.width) / 2;
+//    CGFloat labelY1 = (numberViewBounds.size.height - numberViewFrame.size.height) / 2;
+//    
+//    
+//    CGRect viewFrame = CGRectMake(labelX1, labelY1,
+//                                 numberViewFrame.size.width, numberViewFrame.size.height);
+//    
+//    numberLabel.frame = viewFrame;
+    
+    // Scrollview
+    
+    
+
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
