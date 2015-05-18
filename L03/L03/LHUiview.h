e6e85fc7152291b5caaf0529cecd7bf98e66eba6
LawrenceHan <yangfei6565@163.com>
2015-05-18 11:03:40 +0800

    Added custom UIView. Added scroll view.

---
+++ L03/L03/LHUiview.h
@@ -0,0 +1,13 @@
+//
+//  LHUiview.h
+//  L03
+//
+//  Created by Hanguang on 5/18/15.
+//  Copyright (c) 2015 Hanguang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LHUiview : UIView
+
+@end
---
+++ L03/L03/LHUiview.m
@@ -0,0 +1,30 @@
+//
+//  LHUiview.m
+//  L03
+//
+//  Created by Hanguang on 5/18/15.
+//  Copyright (c) 2015 Hanguang. All rights reserved.
+//
+
+#import "LHUiview.h"
+
+@implementation LHUiview
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
