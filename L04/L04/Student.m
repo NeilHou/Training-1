8ae82d65f3eb905dd06f3b8659d7a4d73efa286e
MrMessy77 <mrmessy@billys-MacBook-Pro.local>
2015-05-22 20:32:39 +0800

    生成4个对应的students

--- L04/L04/Student.m
+++ L04/L04/Student.m
@@ -22,41 +22,41 @@
     return descriptionString;
 }
 
-+(instancetype)randomItem{
-    
-    NSArray *nameNumber = @[@"朱剑波",@"张三",@"李四"];
-    NSArray *randomAge = @[@"23",@"24",@"25"];
-    NSArray *randomId = @[@"1006030",@"1006040",@"1006050"];
-    NSArray *classNumber = @[@"一年级",@"二年级",@"三年级"];
-    NSArray *hobbyNumber = @[@"篮球",@"足球",@"网球"];
-    NSArray *studentImage = @[@"student_01.JPG",@"student_02.JPG",@"student_03.JPG",@"student_04.JPG"];
-    
-    NSInteger nameIndex = arc4random() % [nameNumber count];
-    NSInteger ageIndex = arc4random() % [randomAge count];
-    NSInteger idIndex = arc4random() % [randomId count];
-    NSInteger classIndex = arc4random() % [classNumber count];
-    NSInteger hobbyIndex = arc4random() % [hobbyNumber count];
-    NSInteger imageIndex = arc4random() % [studentImage count];
-    
-    NSString *randomNameNumber = [NSString stringWithFormat:@"%@",nameNumber[nameIndex]];
-    
-    NSString *randomAgeNumber = [NSString stringWithFormat:@"%@",randomAge[ageIndex]];
-    
-    NSString *randomIdNumber = [NSString stringWithFormat:@"%@",
-                                randomId[idIndex]];
-    NSString *randomClassNumber = [NSString stringWithFormat:@"%@",classNumber[classIndex]];
-    
-    NSString *randomHobbyNumber = [NSString stringWithFormat:@"%@",hobbyNumber[hobbyIndex]];
-    NSString *randomStudentImage = [NSString stringWithFormat:@"%@",studentImage[imageIndex]];
-    
-    Student *jb = [[Student alloc]init];
-    jb.name = randomNameNumber;
-    jb.age = randomAgeNumber;
-    jb.studentId = randomIdNumber;
-    jb.studentClass = randomClassNumber;
-    jb.hobby = randomHobbyNumber;
-    jb.image = [UIImage imageNamed:randomStudentImage];
-
-    return jb;
-}
+//+(instancetype)randomItem{
+//    
+//    NSArray *nameNumber = @[@"朱剑波",@"张三",@"李四"];
+//    NSArray *randomAge = @[@"23",@"24",@"25"];
+//    NSArray *randomId = @[@"1006030",@"1006040",@"1006050"];
+//    NSArray *classNumber = @[@"一年级",@"二年级",@"三年级"];
+//    NSArray *hobbyNumber = @[@"篮球",@"足球",@"网球"];
+//    NSArray *studentImage = @[@"student_01.JPG",@"student_02.JPG",@"student_03.JPG",@"student_04.JPG"];
+//    
+//    NSInteger nameIndex = arc4random() % [nameNumber count];
+//    NSInteger ageIndex = arc4random() % [randomAge count];
+//    NSInteger idIndex = arc4random() % [randomId count];
+//    NSInteger classIndex = arc4random() % [classNumber count];
+//    NSInteger hobbyIndex = arc4random() % [hobbyNumber count];
+//    NSInteger imageIndex = arc4random() % [studentImage count];
+//    
+//    NSString *randomNameNumber = [NSString stringWithFormat:@"%@",nameNumber[nameIndex]];
+//    
+//    NSString *randomAgeNumber = [NSString stringWithFormat:@"%@",randomAge[ageIndex]];
+//    
+//    NSString *randomIdNumber = [NSString stringWithFormat:@"%@",
+//                                randomId[idIndex]];
+//    NSString *randomClassNumber = [NSString stringWithFormat:@"%@",classNumber[classIndex]];
+//    
+//    NSString *randomHobbyNumber = [NSString stringWithFormat:@"%@",hobbyNumber[hobbyIndex]];
+//    NSString *randomStudentImage = [NSString stringWithFormat:@"%@",studentImage[imageIndex]];
+//    
+//    Student *jb = [[Student alloc]init];
+//    jb.name = randomNameNumber;
+//    jb.age = randomAgeNumber;
+//    jb.studentId = randomIdNumber;
+//    jb.studentClass = randomClassNumber;
+//    jb.hobby = randomHobbyNumber;
+//    jb.image = [UIImage imageNamed:randomStudentImage];
+//
+//    return jb;
+//}
 @end
--- L04/L04/ViewController.m
+++ L04/L04/ViewController.m
@@ -52,9 +52,20 @@
         // 生成可变数组
         NSMutableArray *array = [NSMutableArray array];
         // 循环5次 生成5个student
-        for (int i =0; i < 5; i++) {
+        for (int i =0; i < 4; i++) {
             // 初始化 student
-            Student *student = [Student randomItem];
+            NSArray *nameNumber = @[@"鲍远申",@"袁波",@"侯建磊",@"朱剑波"];
+            NSArray *randomAge = @[@"23",@"24",@"25",@"23"];
+            NSArray *randomId = @[@"1006030",@"1006040",@"1006050",@"1006060"];
+            NSArray *classNumber = @[@"一年级",@"二年级",@"三年级",@"四年级"];
+            NSArray *studentImage = @[@"student_01.JPG",@"student_02.JPG",@"student_03.JPG",@"student_04.JPG"];
+            
+            Student *student = [[Student alloc] init];
+            student.name = nameNumber[i];
+            student.age = randomAge[i];
+            student.studentId = randomId[i];
+            student.studentClass = classNumber[i];
+            student.image = [UIImage imageNamed:studentImage[i]];
 
             // 添加到数组
             [array addObject:student];
@@ -78,25 +89,26 @@
     Student *student = self.studentsArray[indexPath.row];
     
     // 自定义cell 信息
-    //设置图片
-    cell.mugshotImageVIew.image = student.image;
-    //设置ID
-    cell.idLabel.text = student.studentId;
-    [cell.idLabel sizeToFit];
     //设置name
     cell.nameLabel.text = student.name;
     [cell.nameLabel sizeToFit];
-    //设置class
-    cell.classLabel.text = student.studentClass;
-    [cell.classLabel sizeToFit];
     //设置age
     cell.ageLabel.text = student.age;
     [cell.ageLabel sizeToFit];
+    //设置ID
+    cell.idLabel.text = student.studentId;
+    [cell.idLabel sizeToFit];
+    //设置class
+    cell.classLabel.text = student.studentClass;
+    [cell.classLabel sizeToFit];
+     //设置图片
+    cell.mugshotImageVIew.image = student.image;
     //设置时间
-    cell.timeLabel.text = [[NSDate date] descriptionWithLocale:[NSLocale currentLocale]];// stringWithFormat
+//  cell.timeLabel.text = [[NSDate date] descriptionWithLocale:[NSLocale currentLocale]];
+    cell.timeLabel.text = [NSString stringWithFormat:@"%@",[NSDate date]];// stringWithFormat
     [cell.timeLabel sizeToFit];
     
-    //设置字体大小
+    //设置字体大小[
 //    cell.textLabel.font = [UIFont systemFontOfSize:30];
     
     //设置cell高度
