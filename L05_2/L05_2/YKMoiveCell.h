10f5b04d26879357782e095fbf482470e2fda802
wyk111wyk <Amos@AmosdeMacBook-Pro-15.local>
2015-06-01 11:30:00 +0800

    修复了cell的nib的bug！！！

--- L05_2/L05_2/YKMoiveCell.h
+++ L05_2/L05_2/YKMoiveCell.h
@@ -9,6 +9,14 @@
 #import <UIKit/UIKit.h>
 
 @interface YKMoiveCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *images;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *original_titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *directorsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *castsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *durationsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *pubdatesLabel;
+@property (weak, nonatomic) IBOutlet UILabel *ratingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rating_countLabel;
 
 @end
