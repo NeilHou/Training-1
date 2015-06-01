04ba356163e02901f0566759154c16496c06e1cb
wyk111wyk <Amos@AmosdeMacBook-Pro-15.local>
2015-06-01 20:26:15 +0800

    修改Detail的属性

--- L05_2/L05_2/YKDetailViewController.m
+++ L05_2/L05_2/YKDetailViewController.m
@@ -9,6 +9,16 @@
 #import "YKDetailViewController.h"
 
 @interface YKDetailViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *images;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *popularityLabel;
+@property (weak, nonatomic) IBOutlet UILabel *directorsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *castsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *durationsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *release_dateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *votingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *voting_countLabel;
+@property (weak, nonatomic) IBOutlet UILabel *overviewLabel;
 
 @end
 
