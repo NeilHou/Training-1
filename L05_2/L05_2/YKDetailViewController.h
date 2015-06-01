17f534324f86ee18118bf8516944fe5d39c56973
wyk111wyk <Amos@AmosdeMacBook-Pro-15.local>
2015-06-01 20:18:13 +0800

    关联Detail的各个属性，并调整autolayout

--- L05_2/L05_2/YKDetailViewController.h
+++ L05_2/L05_2/YKDetailViewController.h
@@ -9,5 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface YKDetailViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *images;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *popularityLabel;
+@property (weak, nonatomic) IBOutlet UILabel *directorsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *castsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *durationsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *release_dateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *votingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *voting_countLabel;
+@property (weak, nonatomic) IBOutlet UILabel *overviewLabel;
 
 @end
