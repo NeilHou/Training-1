e9925e2b546620be5ce8dca7c19987e35aadb4c5
wyk111wyk <Amos@AmosdeMacBook-Pro-15.local>
2015-06-02 08:12:48 +0800

    New init

---
+++ L05_03/L05_03/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  L05_03
+//
+//  Created by Amos Wu on 15/6/1.
+//  Copyright (c) 2015年 Amos Wu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
